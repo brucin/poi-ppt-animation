3c95a1696ad2c3f931d3ce2949f78f94c4076ca4
wangjun <wangjun@maptalks.org>
2016-08-08 11:16:37 +0800

    add ZoomIn/ZoomOut animation effect.

---
+++ src/main/java/org/maptalks/poi/animation/out/ZoomOut.java
@@ -0,0 +1,189 @@
+package org.maptalks.poi.animation.out;
+
+import org.apache.poi.xslf.usermodel.XSLFSimpleShape;
+import org.apache.xmlbeans.XmlException;
+import org.maptalks.poi.animation.XSLFAnimation;
+import org.maptalks.poi.animation.XSLFAnimationType;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Text;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+/**
+ * Created by wangjun on 16/2/18.
+ */
+public class ZoomOut extends XSLFAnimationType {
+
+    public ZoomOut(XSLFSimpleShape shape, String moveDirection) {
+        this.shape = shape;
+        this.moveDirection = moveDirection;
+        this.nodeType = "clickEffect";
+    }
+
+    public ZoomOut(XSLFSimpleShape shape, String moveDirection, String nodeType) {
+        this.shape = shape;
+        this.moveDirection = moveDirection;
+        this.nodeType = nodeType;
+    }
+
+    public Element toXml(Document document, XSLFAnimation animation) throws XmlException, ParserConfigurationException {
+        String spid = Integer.toString(this.shape.getShapeId());
+        
+        Element element4 = document.createElementNS(XSLFAnimation.NS,"p:par");
+        Element element5 = document.createElementNS(XSLFAnimation.NS,"p:cTn");
+        element5.setAttribute("id", animation.getElementIdStr());
+        element5.setAttribute("fill", "hold");
+
+        Element element6 = document.createElementNS(XSLFAnimation.NS,"p:stCondLst");
+        Element element7 = document.createElementNS(XSLFAnimation.NS,"p:cond");
+        element7.setAttribute("delay", "0");
+        element6.appendChild(element7);
+        element5.appendChild(element6);
+        element4.appendChild(element5);
+        
+        /**
+         * <p:childTnLst>
+              <p:par>
+                <p:cTn id="5" presetID="2" presetClass="entr" presetSubtype="8" fill="hold" nodeType="clickEffect">
+                  <p:stCondLst>
+                    <p:cond delay="0"/>
+                  </p:stCondLst>
+                  <p:childTnLst>
+         */
+        Element childList2 = document.createElementNS(XSLFAnimation.NS,"p:childTnLst");
+        Element element21 = document.createElementNS(XSLFAnimation.NS,"p:par");
+        Element element22 = document.createElementNS(XSLFAnimation.NS,"p:cTn");
+        element22.setAttribute("id", animation.getElementIdStr());
+        element22.setAttribute("presetID", spid);
+        element22.setAttribute("presetClass", "entr");
+        element22.setAttribute("presetSubtype", "16");
+        element22.setAttribute("fill", "hold");
+        element22.setAttribute("nodeType", this.nodeType);
+        
+        Element element23 = document.createElementNS(XSLFAnimation.NS,"p:stCondLst");
+        Element element24 = document.createElementNS(XSLFAnimation.NS,"p:cond");
+        element24.setAttribute("delay", "0");
+        element23.appendChild(element24);
+        element22.appendChild(element23);
+        element21.appendChild(element22);
+        childList2.appendChild(element21);
+        
+        Element childList3 = document.createElementNS(XSLFAnimation.NS,"p:childTnLst");
+        Element pSet = document.createElementNS(XSLFAnimation.NS,"p:set");
+        Element pcBhvr = document.createElementNS(XSLFAnimation.NS,"p:cBhvr");
+        Element element31 = document.createElementNS(XSLFAnimation.NS,"p:cTn");
+        element31.setAttribute("id", animation.getElementIdStr());
+        element31.setAttribute("fill", "hold");
+        element31.setAttribute("dur", "1");
+        Element element32 = document.createElementNS(XSLFAnimation.NS,"p:stCondLst");
+        Element element33 = document.createElementNS(XSLFAnimation.NS,"p:cond");
+        element33.setAttribute("delay", "500");
+        element32.appendChild(element33);
+        element31.appendChild(element32);
+        pcBhvr.appendChild(element31);
+        
+
+        Element pTgtEl = document.createElementNS(XSLFAnimation.NS,"p:tgtEl");
+        Element pSpTgt = document.createElementNS(XSLFAnimation.NS,"p:spTgt");
+        pSpTgt.setAttribute("spid", spid);//shapeId
+        pTgtEl.appendChild(pSpTgt);
+        pcBhvr.appendChild(pTgtEl);
+        
+        Element attrNameLst = document.createElementNS(XSLFAnimation.NS,"p:attrNameLst");
+        Element attrName = document.createElementNS(XSLFAnimation.NS,"p:attrName");
+        Text text = document.createTextNode("style.visibility");
+        attrName.appendChild(text);
+        attrNameLst.appendChild(attrName);
+        pcBhvr.appendChild(attrNameLst);
+        
+        Element pTo = document.createElementNS(XSLFAnimation.NS,"p:to");
+        Element pStrVal = document.createElementNS(XSLFAnimation.NS,"p:strVal");
+        pStrVal.setAttribute("val", "visible");
+        pTo.appendChild(pStrVal);
+        pSet.appendChild(pcBhvr);
+        pSet.appendChild(pTo);
+        childList3.appendChild(pSet);
+        
+        childList3.appendChild(createAnimationXml(animation,document, spid, "ppt_w"));
+        childList3.appendChild(createAnimationXml(animation,document, spid, "ppt_h"));
+        element22.appendChild(childList3);
+        element5.appendChild(childList2);
+//        firstElement.appendChild(element1);
+//        root.appendChild(firstElement);
+        return element4;
+    }
+    
+    public Element createAnimationXml(XSLFAnimation animation,Document document, String spid, String locTag) {
+        Element panim = document.createElementNS(XSLFAnimation.NS,"p:anim");
+        panim.setAttribute("calcmode", "lin");
+        panim.setAttribute("valueType", "num");
+        
+        Element pcBhvr = document.createElementNS(XSLFAnimation.NS,"p:cBhvr");
+        pcBhvr.setAttribute("additive", "base");
+        Element pcTn = document.createElementNS(XSLFAnimation.NS,"p:cTn");
+        pcTn.setAttribute("id", animation.getElementIdStr());
+        pcTn.setAttribute("dur", "1500");
+        pcTn.setAttribute("fill", "hold");
+        pcBhvr.appendChild(pcTn);
+        
+        Element pTgtEl = document.createElementNS(XSLFAnimation.NS,"p:tgtEl");
+        Element pSpTgt = document.createElementNS(XSLFAnimation.NS,"p:spTgt");
+        pSpTgt.setAttribute("spid", spid);//shapeId
+        pTgtEl.appendChild(pSpTgt);
+        pcBhvr.appendChild(pTgtEl);
+        
+        Element attrNameLst = document.createElementNS(XSLFAnimation.NS,"p:attrNameLst");
+        Element attrName = document.createElementNS(XSLFAnimation.NS,"p:attrName");
+        Text text = document.createTextNode(locTag);
+        attrName.appendChild(text);
+        attrNameLst.appendChild(attrName);
+        pcBhvr.appendChild(attrNameLst);
+        panim.appendChild(pcBhvr);
+        
+        Element ptavLst = document.createElementNS(XSLFAnimation.NS,"p:tavLst");
+        String startLoc = this.getStartLocation(locTag);
+        String endLoc = this.getEndLocation(locTag);
+        ptavLst.appendChild(createShapeLocation(document, "0", startLoc));
+        ptavLst.appendChild(createShapeLocation(document, "100000", endLoc));
+        panim.appendChild(ptavLst);
+        return panim;
+    }
+    
+    private String getStartLocation(String locTag) {
+        String result = new String("");
+        if(locTag.equals("ppt_w")) {
+            result = "ppt_w";
+        } else {
+            result = "ppt_h";
+        }
+        return result;
+    }
+    
+    private String getEndLocation(String locTag) {
+        return "0";
+    }
+    
+    /**
+     * <p:tav tm="0">
+          <p:val>
+            <p:strVal val="#ppt_w"/>
+          </p:val>
+        </p:tav>
+     *
+     * @param tm
+     * @param location
+     * @return
+     */
+    public Element createShapeLocation(Document doc, String tm, String location) {
+        Element ptav = doc.createElementNS(XSLFAnimation.NS,"p:tav");
+        ptav.setAttribute("tm", tm);
+        Element pval = doc.createElementNS(XSLFAnimation.NS,"p:val");
+        Element pstrVal = doc.createElementNS(XSLFAnimation.NS,"p:strVal");
+        pstrVal.setAttribute("val", location);
+        pval.appendChild(pstrVal);
+        ptav.appendChild(pval);
+        return ptav;
+    }
+
+}
---
+++ src/test/java/org/maptalks/poi/animation/TestZoomIn.java
@@ -0,0 +1,81 @@
+/* ====================================================================
+   Licensed to the Apache Software Foundation (ASF) under one or more
+   contributor license agreements.  See the NOTICE file distributed with
+   this work for additional information regarding copyright ownership.
+   The ASF licenses this file to You under the Apache License, Version 2.0
+   (the "License"); you may not use this file except in compliance with
+   the License.  You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+==================================================================== */
+package org.maptalks.poi.animation;
+
+import org.apache.poi.xslf.usermodel.XMLSlideShow;
+import org.apache.poi.xslf.usermodel.XSLFPictureData;
+import org.apache.poi.xslf.usermodel.XSLFPictureShape;
+import org.apache.poi.xslf.usermodel.XSLFSlide;
+import org.junit.Test;
+import org.maptalks.poi.animation.in.ZoomIn;
+import org.maptalks.poi.animation.out.FlyOut;
+
+import java.awt.*;
+import java.awt.geom.Rectangle2D;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by wangjun on 16/2/18.
+ */
+public class TestZoomIn {
+    
+    @Test
+    public void testAnimationSlide() throws Exception {
+        XMLSlideShow pptx = new XMLSlideShow();
+        XSLFSlide slide = pptx.createSlide();
+        Dimension pageSize = pptx.getPageSize();
+        Rectangle2D rectangle = new Rectangle2D.Double(0,0,pageSize.getWidth(),pageSize.getHeight());
+        List<XSLFAnimationType> animationTypes = new ArrayList<XSLFAnimationType>();
+        XSLFAnimation animation = new XSLFAnimation();
+        String[] directions = new String[5];
+        directions[0] = MoveDirection.BOTTOM;
+        directions[1] = MoveDirection.LEFT;
+        directions[2] = MoveDirection.TOP;
+        directions[3] = MoveDirection.RIGHT;
+        directions[4] = MoveDirection.BOTTOM;
+        String pathStr = this.getClass().getResource("/images").getPath();
+        for (int i = 0; i < 3; i++) {
+            String fileName = pathStr+"/"+(i+1)+".png";
+            File downloadeFile = new File(fileName);
+            BufferedInputStream in = new  BufferedInputStream(new FileInputStream(downloadeFile));
+            ByteArrayOutputStream out=new ByteArrayOutputStream(1024);
+            int size=0;
+            byte[] temp=new byte[1024];
+            while ((size = in.read(temp)) != -1) {
+                out.write(temp, 0, size);
+            }
+            in.close();
+            byte[] content=out.toByteArray();
+            XSLFPictureData pictureData = pptx.addPicture(content, org.apache.poi.sl.usermodel.PictureData.PictureType.PNG);
+            XSLFPictureShape picShape=slide.createPicture(pictureData);
+            picShape.setAnchor(rectangle);
+            picShape.setLineWidth(0);
+            XSLFAnimationType animationType = new ZoomIn(picShape, directions[i]);
+            animationTypes.add(animationType);
+
+            XSLFAnimationType animationOutType = new FlyOut(picShape, directions[i]);
+            animationTypes.add(animationOutType);
+        }
+        slide = animation.addAnimationToSlide(slide, animationTypes); 
+        FileOutputStream out = new FileOutputStream(pathStr+"/zoomin.pptx");
+        pptx.write(out);
+        out.close();   
+    }
+   
+}
